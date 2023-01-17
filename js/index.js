e1d62555bc2fe0683d4747f13aceb895016b9273
andrebono <greatone0520@minigate.net>
2023-01-17 13:16:14 +0900

    코드 수정

--- js/index.js
+++ js/index.js
@@ -1,13 +1,7 @@
 let main = document.querySelector('.main');
 let img = document.querySelector('.img');
 
-window.addEventListener("resize", function(){
-    if (window.innerHeight < 200) {
-        main.style.height = "200px";
-    } else {
-        main.style.height = "100vh";
-    }
-
+function greatSize() {
     if (window.innerWidth / window.innerHeight < 3408 / 2480) {
         img.style.width = "100%";
         img.style.height = "auto";
@@ -15,4 +9,10 @@ window.addEventListener("resize", function(){
         img.style.width = "auto";
         img.style.height = "100%";
     }
+}
+
+greatSize();
+
+window.addEventListener("resize", function(){
+    greatSize();
 });
