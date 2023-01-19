da95bbb46e2f67bfc3d21ea953b2620821f1be40
andrebono <greatone0520@minigate.net>
2023-01-19 10:51:42 +0900

    샤크의 코드 수정

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 let main = document.querySelector('.main');
 let img = document.querySelector('.img');
+let header = document.querySelector('h1');
 
 function greatSize() {
-    if (window.innerWidth / window.innerHeight < 3408 / 2480) {
+    if (window.innerWidth / (window.innerHeight - 65) < 3408 / 2480) {
         img.style.width = "100%";
         img.style.height = "auto";
     } else {
@@ -17,7 +18,7 @@ window.addEventListener("resize", function(){
     if (window.innerHeight < 200) {
         main.style.height = "200px";
     } else {
-        main.style.height = "100vh";
+        main.style.height = Number(window.innerHeight - 65) + "px";
     }
     greatSize();
 });
