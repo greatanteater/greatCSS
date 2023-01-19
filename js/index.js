d69bc3476d3000e7605042c2c3490799dc802a22
andrebono <greatone0520@minigate.net>
2023-01-19 16:15:09 +0900

    코드 수정

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ let img = document.querySelector('.img');
 let header = document.querySelector('h1');
 
 function greatSize() {
-    if (window.innerWidth / (window.innerHeight - 65) < 3408 / 2480) {
+    
+    if (main.offsetWidth / main.offsetHeight < 3508 / 2480) {
         img.style.width = "100%";
         img.style.height = "auto";
     } else {
@@ -18,7 +19,7 @@ window.addEventListener("resize", function(){
     if (window.innerHeight < 200) {
         main.style.height = "200px";
     } else {
-        main.style.height = Number(window.innerHeight - 65) + "px";
+        main.style.height = "calc(100vh - 65px)";
     }
     greatSize();
 });
